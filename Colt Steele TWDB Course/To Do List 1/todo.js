47bc827192eded94a08f8cd3d2b0d34a8a0b2f6b
Aniket Jha <imanik8@hotmail.com>
2020-06-03 14:00:37 +0530

    todo-1-modified

--- Colt Steele TWDB Course/To Do List 1/todo.js
+++ Colt Steele TWDB Course/To Do List 1/todo.js
@@ -4,12 +4,36 @@ var input = prompt("What would you like to do??");
 
 while(input !== "quit"){
     if(input==="list"){
-        console.log(todos);
+        listTodo();
     }
     else if(input==="new"){
-        var newTodo = prompt("Enter new todo");
-        todos.push(newTodo);
+        addTodo();
+    }
+    else if(input==="delete"){
+        deleteTodo();
     }
     input = prompt("What would you like to do??");
 }
 console.log("You Quit the APP..Fuck OFF!!");
+
+function listTodo(){
+	console.log("**********");
+	todos.forEach(function(todo, index){
+		console.log(index + ": " + todo);
+	});
+	console.log("**********");
+}
+
+function addTodo(){
+	//ask for new todo
+	var newTodo = prompt("Enter new todo");
+	//add to todos array
+	todos.push(newTodo);
+	console.log(newTodo + " added to list")
+}
+
+function deleteTodo(){
+	var index = prompt("Enter index of todo to delete");
+	todos.splice(index, 1);
+	console.log("Todo Removed")
+}
