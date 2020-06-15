666c38fc6e92618aebb3e352eaff7477392c8e8b
Aniket Jha <imanik8@hotmail.com>
2020-06-15 18:04:19 +0530

    coding-challenge-6-part-2

--- Jonas TCJS/DOM Pig Game/app.js
+++ Jonas TCJS/DOM Pig Game/app.js
@@ -51,8 +51,17 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
     //update the UI
     document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
 
+    var input = document.querySelector(".final-score").value;
+    var winningScore;
+
+    if(input){
+        winningScore = input;
+    } else{
+        winningScore = 100;
+    }
+
     //check if player won the game
-    if(scores[activePlayer] >= 20){
+    if(scores[activePlayer] >= winningScore){
         document.querySelector("#name-" + activePlayer).textContent = 'Winner!'; 
         document.querySelector(".dice").style.display = "none";
         document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
