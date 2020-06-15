2d08b8466805f3b3f50161004680f3732ecdd94a
Aniket Jha <imanik8@hotmail.com>
2020-06-15 17:52:37 +0530

    coding-challenge-6-part-1

--- Jonas TCJS/DOM Pig Game/app.js
+++ Jonas TCJS/DOM Pig Game/app.js
@@ -11,6 +11,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer, gamePlaying;
 init();
+var lastDice;
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
 
@@ -24,7 +25,12 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     diceDOM.src = 'dice-' + dice + '.png';
 
     //3. Update the round score if the rolled number was not a 1
-    if(dice !== 1){
+    if(dice === 6 && lastDice === 6){
+        //player looses score
+        scores[activePlayer] = 0;
+        document.querySelector("#score-" + activePlayer).textContent = '0';
+        nextPlayer();
+    } else if(dice !== 1){
         //Add Score
         roundScore += dice;
         document.querySelector("#current-" + activePlayer).textContent = roundScore;
@@ -32,6 +38,7 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         //Next Player
         nextPlayer();
     }
+    lastDice = dice;
     }
 })
 
