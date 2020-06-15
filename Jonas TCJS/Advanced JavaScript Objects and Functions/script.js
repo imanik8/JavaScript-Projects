277aa75f2992324f854a6096a532f9a85efa2009
Aniket Jha <imanik8@hotmail.com>
2020-06-15 22:15:06 +0530

    object-vs-primitives

--- Jonas TCJS/Advanced JavaScript Objects and Functions/script.js
+++ Jonas TCJS/Advanced JavaScript Objects and Functions/script.js
@@ -48,3 +48,35 @@ var jane = Object.create(personProto,
     });
 */
 
+//Primitives Vs Objects
+ var a = 23;
+ var b = a;
+ a = 43;
+ console.log(a);
+ console.log(b);
+
+ //Object
+ var obj1 = {
+     name: 'John',
+     age: 26
+ };
+ var obj2 = obj1;
+ obj1.age = 30;
+ console.log(obj1.age);
+ console.log(obj2.age);
+
+ //Function
+ var age = 27;
+ var obj = {
+     name: 'Jonas',
+     city: 'Lisbon'
+ };
+
+ function change(a, b){
+     a = 30;
+     b.city = 'San Francissco';
+ }
+
+ change(age, obj);
+ console.log(age);
+ console.log(obj.city);
