861c757be4cbe86649309ab81f8b06a72724ac72
Aniket Jha <imanik8@hotmail.com>
2020-06-15 21:03:22 +0530

    function-constructor

---
+++ Jonas TCJS/Advanced JavaScript Objects and Functions/script.js
@@ -0,0 +1,25 @@
+// Function COnstructor
+
+var Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person.prototype.calculateAge = function(){
+    console.log(2016 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1998, 'teacher');
+var himanshu = new Person('Himanshu', 1968, 'Engineer');
+var saket = new Person('Saket', 1948, 'Doctor');
+
+john.calculateAge();
+himanshu.calculateAge();
+saket.calculateAge();
+
+console.log(john.lastName);
+console.log(himanshu.lastName);
+console.log(saket.lastName);
