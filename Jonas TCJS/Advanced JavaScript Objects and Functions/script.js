0da3c13b8d62dd15505bf9054f8e05cdbf0ac74a
Aniket Jha <imanik8@hotmail.com>
2020-06-16 20:48:56 +0530

    closure

--- Jonas TCJS/Advanced JavaScript Objects and Functions/script.js
+++ Jonas TCJS/Advanced JavaScript Objects and Functions/script.js
@@ -167,7 +167,6 @@ var jane = Object.create(personProto,
  }
 
  game();
- */
 
  (function () {
      var score = Math.random() * 10;
@@ -180,3 +179,45 @@ var jane = Object.create(personProto,
     var score = Math.random() * 10;
     console.log(score >= 5);
 })(5);
+*/
+
+// CLosure
+
+function retirement(retirementAge){
+    var a = ' years left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementUS(1990);
+retirementGermany(1990);
+retirementIceland(1990);
+
+
+function jobInterview(job){
+    return function(name){
+        if(job === 'designer'){
+            console.log(name + ', can you please explain what UX design is?');
+        }
+        else if(job === 'teacher'){
+            console.log('What subject do you teach, ' + name + '?');
+        }
+        else{
+            console.log('Hello ' + name + ', What do you do??');
+        }
+    }
+}
+
+var teacherQuestion = jobInterview('teacher');
+var designerQuestion = jobInterview('designer');
+var whoRU = jobInterview('wru');
+
+teacherQuestion('mary');
+designerQuestion('Aniket');
+whoRU('Shreya');
