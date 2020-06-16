56ff6e63788d6cf08fb048b041b676a1b193736f
Aniket Jha <imanik8@hotmail.com>
2020-06-16 21:19:54 +0530

    bind-call-apply

--- Jonas TCJS/Advanced JavaScript Objects and Functions/script.js
+++ Jonas TCJS/Advanced JavaScript Objects and Functions/script.js
@@ -183,6 +183,8 @@ var jane = Object.create(personProto,
 
 // CLosure
 
+/*
+
 function retirement(retirementAge){
     var a = ' years left until retirement.';
     return function(yearOfBirth) {
@@ -221,3 +223,68 @@ var whoRU = jobInterview('wru');
 teacherQuestion('mary');
 designerQuestion('Aniket');
 whoRU('Shreya');
+
+*/
+
+// Bind, call and apply
+
+var john = {
+    name: 'John',
+    age: 26,
+    job: 'teacher',
+    presentation: function(style, timeOfDay) {
+        if(style === 'formal'){
+            console.log('Good ' + timeOfDay + ', Ladies and Gentleman! I\'m ' + this.name + ', I\'m a ' + this.job + 'and I\'m ' + this.age + ' years old.');
+        } else if(style === 'friendly') {
+            console.log('Hey! What\'s up?? I\'m ' + this.name + ', I\'m a ' + this.job + 'and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+        }
+    }
+}
+
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+}
+
+john.presentation('formal','morning');
+
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+//apply method
+//john.presentation.apply(emily,['friendly', 'afternoon']);
+
+//bind method
+
+var johnFriendly = john.presentation.bind(john, 'friendly');
+johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+emilyFormal('morning');
+emilyFormal('night');
+
+
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+ function arrayCalc(arr, fn) {
+     var arrRes = [];
+     for(var i=0; i<arr.length; i++){
+         arrRes.push(fn(arr[i]));
+     }
+     return arrRes;
+ }
+
+ function calculateAge(el) {
+     return 2020 - el;
+ }
+
+ function isFullAge(limit, el){
+     return el >= limit;
+ }
+
+ var ages = arrayCalc(years, calculateAge);
+ var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+ console.log(ages);
+ console.log(fullJapan);
