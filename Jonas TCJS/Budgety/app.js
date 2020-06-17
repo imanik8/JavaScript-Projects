ec2b8391e777885f8e4220fb53e5c28ba49ef802
Aniket Jha <imanik8@hotmail.com>
2020-06-17 17:20:07 +0530

    clearing-our-input-fields

--- Jonas TCJS/Budgety/app.js
+++ Jonas TCJS/Budgety/app.js
@@ -102,6 +102,20 @@ var UiController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+        clearFields: function() {
+            var fields, fieldsArr;
+            
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+            
+            fieldsArr = Array.prototype.slice.call(fields);
+            
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+            
+            fieldsArr[0].focus();
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
