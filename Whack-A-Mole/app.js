b49c633ab277afa48c89d7168481e6249923dbf9
Aniket Jha <imanik8@hotmail.com>
2020-06-22 00:58:03 +0530

    whack-a-mole-game-done

--- Whack-A-Mole/app.js
+++ Whack-A-Mole/app.js
@@ -1,4 +1,3 @@
-
 const square = document.querySelectorAll('.square')
 const mole = document.querySelectorAll('.mole')
 const timeLeft = document.querySelector('#time-left')
@@ -19,32 +18,32 @@ function randomSquare() {
 }
 
 square.forEach(id => {
-    id.addEventListener('mouseup', () => {
-      if(id.id === hitPosition){
-        result = result + 1
-        score.textContent = result
-        hitPosition=null
-      }
-    })
-  })
-  
-  
-  function moveMole() {
-    let timerId = null
-    timerId = setInterval(randomSquare, 500)
-  }
-  
-  moveMole()
-
-
-  function countDown() {
-    currentTime--
-    timeLeft.textContent = currentTime
-  
-    if(currentTime === 0 ) {
-      clearInterval(timerId)
-      alert('GAME OVER! Your final score is' + result)
+  id.addEventListener('mouseup', () => {
+    if(id.id === hitPosition){
+      result = result + 1
+      score.textContent = result
+      hitPosition=null
     }
+  })
+})
+
+
+function moveMole() {
+  let timerId = null
+  timerId = setInterval(randomSquare, 500)
+}
+
+moveMole()
+
+
+function countDown() {
+  currentTime--
+  timeLeft.textContent = currentTime
+
+  if(currentTime === 0 ) {
+    clearInterval(timerId)
+    alert('GAME OVER! Your final score is' + result)
   }
-  
-  let timerId = setInterval(countDown, 1000)
+}
+
+let timerId = setInterval(countDown, 1000)
