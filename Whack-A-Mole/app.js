33a9d6fdd453bc36af5831a884e52275ecaaf8ab
Aniket Jha <imanik8@hotmail.com>
2020-06-22 00:51:56 +0530

    random-square-function-added

---
+++ Whack-A-Mole/app.js
@@ -0,0 +1,19 @@
+
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelector('#time-left')
+let score = document.querySelector('#score')
+
+let result = 0
+let currentTime = timeLeft.textContent
+
+function randomSquare() {
+  square.forEach(className => {
+    className.classList.remove('mole')
+  })
+  let randomPosition = square[Math.floor(Math.random() * 9)]
+  randomPosition.classList.add('mole')
+
+  //assign the id of the randomPosition to hitPosition for us to use later
+  hitPosition = randomPosition.id
+}
