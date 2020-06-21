0b6283a0fa37e74be41b5282d2c988b3ad5c55e3
Aniket Jha <imanik8@hotmail.com>
2020-06-22 01:30:11 +0530

    startgame-func-added

--- Nokia 3310 Snake Game/app.js
+++ Nokia 3310 Snake Game/app.js
@@ -14,20 +14,39 @@ document.addEventListener('DOMContentLoaded', () => {
     let interval = 0
 
 
-//assign functions to keycodes
-  function control(e) {
-    squares[currentIndex].classList.remove('snake') //we are removing the class of snake from ALL the squares.
-
-    if(e.keyCode === 39) {
-      direction = 1 //if we press the right arrow on our keyboard, the snake will go right one
-    } else if (e.keyCode === 38) {
-      direction = -width // if we press the up arrow, the snake will go back ten divs, appearing to go up
-    } else if (e.keyCode === 37) {
-      direction = -1 // if we press left, the snake will go left one div
-    } else if (e.keyCode === 40) {
-      direction = +width //if we press down, the snake head will instantly appear in the div ten divs from where you are now
+    
+    //to start, and restart the game
+    function startGame() {
+        currentSnake.forEach(index => squares[index].classList.remove('snake'))
+        squares[appleIndex].classList.remove('apple')
+        clearInterval(interval)
+        score = 0
+        randomApple()
+        direction = 1
+        scoreDisplay.innerText = score
+        intervalTime = 1000
+        currentSnake = [2,1,0]
+        currentIndex = 0
+        currentSnake.forEach(index => squares[index].classList.add('snake'))
+        interval = setInterval(moveOutcomes, intervalTime)
     }
-  }
 
-  document.addEventListener('keyup', control)
-})
+
+    //assign functions to keycodes
+    function control(e) {
+        squares[currentIndex].classList.remove('snake') //we are removing the class of snake from ALL the squares.
+
+        if(e.keyCode === 39) {
+        direction = 1 //if we press the right arrow on our keyboard, the snake will go right one
+        } else if (e.keyCode === 38) {
+        direction = -width // if we press the up arrow, the snake will go back ten divs, appearing to go up
+        } else if (e.keyCode === 37) {
+        direction = -1 // if we press left, the snake will go left one div
+        } else if (e.keyCode === 40) {
+        direction = +width //if we press down, the snake head will instantly appear in the div ten divs from where you are now
+        }
+    }
+
+    document.addEventListener('keyup', control)
+    startBtn.addEventListener('click', startGame)
+    })
