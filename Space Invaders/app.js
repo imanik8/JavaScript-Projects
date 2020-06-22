a709c36b04bf98ae3de7904124c24cb572340455
Aniket Jha <imanik8@hotmail.com>
2020-06-22 11:14:33 +0530

    event-and-var-declaration

---
+++ Space Invaders/app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const resultDisplay = document.querySelector('#result')
+    let width = 15
+    let currentShooterIndex = 202
+    let currentInvaderIndex = 0
+    let alienInvadersTakenDown = []
+    let result = 0
+    let direction = 1
+    let invaderId
+})
