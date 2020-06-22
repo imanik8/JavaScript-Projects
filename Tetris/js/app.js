74087c1a3a012175b6b4532b24848620ffa63aae
Aniket Jha <imanik8@hotmail.com>
2020-06-22 11:55:45 +0530

    create-grid

--- Tetris/js/app.js
+++ Tetris/js/app.js
@@ -27,4 +27,43 @@ document.addEventListener('DOMContentLoaded' () => {
     'url(images/peach_block.png)',
     'url(images/yellow_block.png)'
   ]
+
+
+  function createGrid() {
+    // the main grid
+    let grid = document.querySelector(".grid")
+    for (let i = 0; i < GRID_SIZE; i++) {
+      let gridElement = document.createElement("div")
+      grid.appendChild(gridElement)
+    }
+
+    // set base of grid
+    for (let i = 0; i < GRID_WIDTH; i++) {
+      let gridElement = document.createElement("div")
+      gridElement.setAttribute("class", "block3")
+      grid.appendChild(gridElement)
+    }
+
+    let previousGrid = document.querySelector(".previous-grid")
+    // Since 16 is the max grid size in which all the Tetrominoes 
+    // can fit in we create one here
+    for (let i = 0; i < 16; i++) {
+      let gridElement = document.createElement("div")
+      previousGrid.appendChild(gridElement);
+    }
+    return grid;
+  }
+
+
+  //assign functions to keycodes
+  function control(e) {
+    if (e.keyCode === 39)
+      moveright()
+    else if (e.keyCode === 38)
+      rotate()
+    else if (e.keyCode === 37)
+      moveleft()
+    else if (e.keyCode === 40)
+      moveDown()
+  }  
 })
