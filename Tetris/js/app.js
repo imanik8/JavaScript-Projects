9e39dc57d1da857e8ed7d3b9d1ceabd1ac138888
Aniket Jha <imanik8@hotmail.com>
2020-06-22 11:53:38 +0530

    event-and-var-declaration

---
+++ Tetris/js/app.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded' () => {
+
+    const GRID_WIDTH = 10
+    const GRID_HEIGHT = 20
+    const GRID_SIZE = GRID_WIDTH * GRID_HEIGHT
+
+      // no need to type 200 divs :)
+  const grid = createGrid();
+  let squares = Array.from(grid.querySelectorAll('div'))
+  const startBtn = document.querySelector('.button')
+  const hamburgerBtn = document.querySelector('.toggler')
+  const menu = document.querySelector('.menu')
+  const span = document.getElementsByClassName('close')[0]
+  const scoreDisplay = document.querySelector('.score-display')
+  const linesDisplay = document.querySelector('.lines-score')
+  let currentIndex = 0
+  let currentRotation = 0
+  const width = 10
+  let score = 0
+  let lines = 0
+  let timerId
+  let nextRandom = 0
+  const colors = [
+    'url(images/blue_block.png)',
+    'url(images/pink_block.png)',
+    'url(images/purple_block.png)',
+    'url(images/peach_block.png)',
+    'url(images/yellow_block.png)'
+  ]
+})
