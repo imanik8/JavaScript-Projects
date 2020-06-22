ff822061f1a5977453db9c7b7289a802bba85486
Aniket Jha <imanik8@hotmail.com>
2020-06-22 11:34:08 +0530

    move-the-cars-and-frogs

--- Frogger/app.js
+++ Frogger/app.js
@@ -12,5 +12,32 @@ document.addEventListener('DOMContentLoaded', () => {
     let currentIndex = 76
     let currentTime = 20
     let timerId
-    
+
+  //move the Frog
+  function moveFrog(e) {
+    squares[currentIndex].classList.remove('frog')
+    switch(e.keyCode) {
+      case 37:
+        if(currentIndex % width !== 0) currentIndex -= 1
+        break
+      case 38:
+        if(currentIndex - width >= 0) currentIndex -= width
+        break
+      case 39:
+        if(currentIndex % width < width - 1) currentIndex += 1
+        break
+      case 40:
+        if (currentIndex + width < width * width) currentIndex += width
+        break
+    }
+    squares[currentIndex].classList.add('frog')
+    lose()
+    win()
+  }
+
+  //move the cars
+  function autoMoveCars() {
+    carsLeft.forEach(carLeft => moveCarLeft(carLeft))
+    carsRight.forEach(carRight => moveCarRight(carRight))
+  }    
 })
