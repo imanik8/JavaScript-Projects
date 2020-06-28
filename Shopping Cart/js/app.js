f88124bab9f028c3983d8c41bbb88da2c70c29ed
Aniket Jha <imanik8@hotmail.com>
2020-06-28 18:47:05 +0530

    cart-item-display

---
+++ Shopping Cart/js/app.js
@@ -0,0 +1,8 @@
+(function(){
+    const cartInfo = document.getElementById('cart-info');
+    const cart = document.getElementById('cart');
+
+    cartInfo.addEventListener('click', function(){
+        cart.classList.toggle('show-cart');
+    })
+})();
